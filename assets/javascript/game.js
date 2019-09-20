94ac96091a4d511e5899a9f0d1db9b6666ba5aa0
Charles Tuggle <tuggle.charles@gmail.com>
2019-09-19 23:54:59 -0400

    another commit-scrapped previous code to start anew as i got lost and confused by my different changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+
+    var wins = 0;
+    var losses =0;
+    var guessesLeft =9;
+    var yourGuesses = [];
+
+    document.onkeyup = function (event) {
+
+        var userGuess = event.key;
+        console.log(userGuess)
+
+        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+        var userChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+
+        if (userChoices.indexOf(userGuess) > -1) {
+            
+            if (userGuess === computerGuess) {
+                wins++;
+                guessesLeft = 9;
+                yourGuesses = [];
+            }
+
+            if (userGuess != computerGuess) {
+                guessesLeft --;
+            }
+
+            if (guessesLeft === 0) {
+                guessesLeft = 9;
+                losses++;
+                yourGuesses = [];
+            }
+
+            guessesLeftText.textContent = "Guesses left: " + guessesLeft;
+            yourGuesses.textContent = "Your guesses thus far: " + yourGuesses;
+            winsText.textContent = "Wins: " + wins;
+            lossesText.textContent = "Losses: " + losses;
+
+        }
+    }; 
